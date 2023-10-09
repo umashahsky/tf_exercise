a8fab248dfcf42f923d63d9a90bb3135c937d3a0
umashahsky <uma.shah@sky.uk>
2023-10-09 16:37:06 +0100

    bucket

--- ex1.tf
+++ ex1.tf
@@ -20,7 +20,7 @@
 provider "aws" {
   profile = "default"
   region  = "eu-west-2"
-}                                                                                                                             
+}                                                                                                                              
 
 resource "aws_s3_bucket" "tf_bucket" {
   bucket = "tf-exercise-bucket"
